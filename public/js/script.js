307b82db51eb18cc84b27f7c041915bab122deb7
Subhasis Pal <palsubhasis100@gmail.com>
2024-09-18 23:20:15 +0530

    IntialBuild_Realtime-Tracking

---
+++ public/js/script.js
@@ -0,0 +1,46 @@
+const socket=io();
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition(
+        (position)=>{
+            const {latitude, longitude} = position.coords;
+            socket.emit("send-location",{latitude, longitude});
+
+        },
+        (error) =>{
+            console.log(error);
+        },
+        {
+            enableHighAccuracy:true,
+            timeout:5000,
+            maximumAge: 0
+        }
+    )
+}
+
+// const map = L.map("map").setView([0,0],10);
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+const markers={};
+
+socket.on("receive-location", (data)=>{
+    const{id, latitude, longitude} =data;
+    map.setView([latitude,longitude], 16)
+
+    if(markers[id]){
+        markers[id].setLang([latitude, longitude]);
+    }else{
+        markers[id]=L.marker([latitude,longitude]).addTo(map)
+    }
+})
+
+socket.on("user-disconnected",(id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+})
